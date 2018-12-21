743953d8b7750090bca4d8793a3aebfff5373fe1
idosakazi123 <idosakazi@gmail.com>
2018-12-21 03:57:00 +0200

    need to do testing

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,9 +1,9 @@
 import * as esprima from 'esprima';
 import * as escodegen from 'escodegen';
 
-const parseCode = (codeToParse) => {
+/*const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
-};
+};*/
 
 const makeTable  = (inputVector, codeToParse) => {
     let table = '';
@@ -11,9 +11,9 @@ const makeTable  = (inputVector, codeToParse) => {
     codeToTable(inputVector,codeToParse).forEach(lineTable => {
         let rowTable = '<tr><td>' + lineTable.row +endOfTheRow;
         if(lineTable.color === 'red' ){
-            rowTable = '<tr bgcolor="red"><td>' + lineTable.row +endOfTheRow;
+            rowTable = '<tr bgcolor=#FF0000><td>' + lineTable.row +endOfTheRow;
         }else if(lineTable.color === 'green'){
-            rowTable = '<tr bgcolor="green"><td>' + lineTable.row +endOfTheRow;
+            rowTable = '<tr bgcolor=#00FF00><td>' + lineTable.row +endOfTheRow;
         }else{
             rowTable = '<tr><td>' + lineTable.row +endOfTheRow;
         }
@@ -166,7 +166,7 @@ function returnStatementIfHandel(obj){
 }
 
 function identifierIfHandel(obj){
-    let objectVariableGlobal;let objectVariableLocal;
+    let objectVariableGlobal;let objectVariableIfLocal;let variableLocal;
     arrayVariables.globalVariablesArray.forEach(variable =>{
         if(variable.name === obj.name ){
             objectVariableGlobal = variable;}
@@ -176,11 +176,24 @@ function identifierIfHandel(obj){
     }
     arrayVariables.localVariableIF.forEach( variable =>{
         if(variable.name === obj.name ){
-            objectVariableLocal = variable;}
+            objectVariableIfLocal = variable;}
     });
-    if(objectVariableLocal !== undefined){
-        return objectVariableLocal.value;
+    if(objectVariableIfLocal !== undefined){
+        return objectVariableIfLocal.value;
     }
+    variableLocal = checkIfVariableInLocalVariable(obj);
+    if(variableLocal !== undefined)
+        return variableLocal.value;
+}
+
+function checkIfVariableInLocalVariable(obj){
+    let variableLocal;
+    arrayVariables.localVariablesArray.forEach(variable =>{
+        if(obj.name === variable.name) {
+            variableLocal = variable;
+        }
+    });
+    return variableLocal;
 }
 
 function expressionStatementIfHandel(obj){
@@ -875,255 +888,6 @@ function getParametersFromFunction(parametersFromFunction){
 
 
 
-/*
-const makeTable = (finalModel) => {
-    let table = '<tr>' + '<th> Line </th>' + '<th> Type </th>' + '<th> Name </th>' + '<th> Condition </th>' + '<th> Value </th>' + '</tr>';
-    finalModel.forEach(modelRow => {
-        table +=( '<tr>' +
-            '<td>' + modelRow.line + '</td>' +
-            '<td>' + modelRow.type + '</td>' +
-            '<td>' + modelRow.name + '</td>' +
-            '<td>' + modelRow.condition + '</td>' +
-            '<td>' + modelRow.value + '</td>' +
-            '</tr>');
-    });
-    return table;
-};
 
-const getElementFromParseCode = (parsedCode) => {
-    //let model = [];
-    model = [];
-    index = 0;
-    raw = 1;
-    ifType ='if statement';
-    parsedCode.body.forEach(obj => {
-        //model[i] =  checkType(obj);
-        checkType(obj);
-
-    });
-    //want to see the model
-
-    return model;
-};
-
-let raw;
-let model;
-let index;
-let ifType;
-
-let typeHandel = {
-    'FunctionDeclaration' : functionDeclarationHandle,
-    'BlockStatement' : blockStatementHandle,
-    'VariableDeclaration' : variableDeclarationHandle,
-    'ExpressionStatement' : expressionStatementHandle,
-    'WhileStatement' :  whileStatementHandle,
-    'IfStatement' : ifStatementHandle,
-    'ReturnStatement' : returnStatementHandle,
-    'ForStatement' : forStatementHandle,
-
-    'Literal' : literalHandel,
-    'Identifier' : identifierHandel,
-    'MemberExpression' : memberExpressionHandel,
-
-    'UnaryExpression' : unaryHandel,
-    'UpdateExpression' : updateHandel,
-
-    'AssignmentExpression': assignmentExpressionHandel,
-    'BinaryExpression' : binaryExpressionHandel,
-
-
-
-};
-
-function checkType(obj){
-    //let elements = [];
-    //elements = typeHandel[obj.type](obj);
-    return typeHandel[obj.type](obj);
-    //return elements;
-}
-
-function functionDeclarationHandle(obj){
-    //let elements = [];
-    model[index] = {line:raw,type:'function declaration',name:obj.id.name,condition:'',value:''};
-    index++;
-    obj.params.forEach(param => {
-        model[index] = {line:raw,type:'variable declaration',name:param.name,condition:'',value:''};
-        index++;
-    });
-    raw++;
-    checkType(obj.body);
-
-    //return elements;
-}
-
-function variableDeclarationHandle(obj){
-    obj.declarations.forEach(variable => {
-        if(variable.init == null){
-            model[index] = {line: raw,type:'variable declaration',name:variable.id.name,condition:'',value:'null'};
-        }else{
-            if(variable.init.type === 'MemberExpression'){
-                model[index] = {line: raw,type:'variable declaration',name:variable.id.name,condition:'',value: checkType(variable.init)};
-            }else{
-                model[index] = {line: raw,type:'variable declaration',name:variable.id.name,condition:'',value:variable.init.value};
-            }
-        }
-        index++;
-    });
-    raw++;
-}
-
-function whileStatementHandle(obj){
-    //let elements = [];
-    let testLeft = checkType(obj.test.left);
-    let testRight = checkType(obj.test.right);
-
-    model[index] = {line:raw,type:'while statement',name:'',condition: testLeft + ' ' + obj.test.operator + ' ' + testRight,value:''};
-    raw++;
-    index++;
-    checkType(obj.body);
-    //return elements;
-}
-
-function ifStatementHandle(obj){
-    let testLeft = checkType(obj.test.left);let testRight = checkType(obj.test.right);
-    model[index] = {line:raw,type:ifType,name:'',condition: testLeft + ' ' + obj.test.operator + ' ' + testRight,value:''};
-    raw++;index++;
-    checkType(obj.consequent);
-    if (obj.alternate != null && obj.alternate.type == 'IfStatement') {
-        ifType = 'else if statement';
-        ifStatementHandle(obj.alternate);
-    } else if(obj.alternate != null) {
-        model[index] = {line: raw, type: 'else statement',name:'',condition:'',value:''};
-        raw++;
-        index++;
-        checkType(obj.alternate);
-    }
-
-
-}
-
-function blockStatementHandle(obj){
-    //let elements = [];
-    //let i =0;
-    obj.body.forEach(param => {
-        checkType(param);
-    });
-    //return elements;
-}
-
-function expressionStatementHandle(obj){
-    return checkType(obj.expression);
-}
-
-function returnStatementHandle(obj){
-    let argument;
-    if(obj.argument != null){
-        argument = checkType(obj.argument);
-    }else{
-        argument = '';
-    }
-    model[index] = {line:raw,type:'return statement',name:'',condition:'',value:argument};
-    raw++;
-    index++;
-}
-
-function forStatementHandle(obj){
-    let testLeft = checkType(obj.test.left);
-    let testRight = checkType(obj.test.right);
-    model[index] = {line:raw,type:'for statement',name:'',condition: testLeft + ' ' + obj.test.operator + ' ' + testRight,value:''};
-    index++;
-    checkInitInForStatement(obj);
-    checkUpdateInForStatement(obj);
-    raw++;
-    checkType(obj.body);
-
-}
-
-function checkInitInForStatement(obj){
-    if(obj.init != null && obj.init.type === 'AssignmentExpression' ){
-        model[index] = {line:raw,type:obj.init.type,name:checkType(obj.init.left),condition:'',value:checkType(obj.init.right)};
-        index++;
-    }else if(obj.init.type === 'VariableDeclaration' ){
-        variableDeclarationInForStatementHandle(obj.init.declarations[0]);
-    }else{
-        model[index] = {line:raw,type:obj.init.type,name:checkType(obj.init),condition:'',value:''};
-        index++;
-    }
-}
-
-function variableDeclarationInForStatementHandle(variable){
-    if(variable.init != null){
-        model[index] = {line: raw,type:'variable declaration',name:variable.id.name,condition:'',value:variable.init.value};
-    }else{
-        model[index] = {line: raw,type:'variable declaration',name:variable.id.name,condition:'',value: 'null'};
-    }
-    index++;
-}
-
-function checkUpdateInForStatement(obj){
-    if(obj.update != null && obj.update.type === 'AssignmentExpression' ){
-        model[index] = {line:raw,type:obj.update.type,name:checkType(obj.update.left),condition:'',value:checkType(obj.update.right)};
-        index++;
-    }else{
-        model[index] = {line: raw, type: obj.update.type, name: checkType(obj.update), condition: '', value: ''};index++;
-    }
-
-}
-
-function literalHandel(literal){
-    return literal.value;
-}
-
-function identifierHandel(identifier){
-    return identifier.name;
-}
-
-function memberExpressionHandel(me){
-    let property = checkType(me.property);
-    let nameProperty = me.object.name;
-
-    return nameProperty + '[' + property + ']';
-}
-
-function unaryHandel(un){
-    let operator = un.operator;
-    let argument = checkType(un.argument);
-    return operator+argument;
-}
-
-function updateHandel(update){
-    let operator = update.operator;
-    let argument = checkType(update.argument);
-    if(!update.prefix){
-        return argument+operator;
-    }else{
-        return operator+argument;
-    }
-
-}
-
-function assignmentExpressionHandel(assignment){
-    let assignmentLeft = checkType(assignment.left);
-    let assignmentRight = checkType(assignment.right);
-    model[index] = {line:raw,type:'assignment expression',name:assignmentLeft,condition:'',value: assignmentRight};
-    raw++;
-    index++;
-}
-
-function binaryExpressionHandel(binary){
-    let operator = binary.operator;
-    let binaryLeft = checkType(binary.left);
-    let binaryRight = checkType(binary.right);
-    if(binary.left.type === 'BinaryExpression'){
-        binaryLeft = '(' + binaryLeft + ')';
-    }
-    if(binary.right.type === 'BinaryExpression'){
-        binaryRight = '(' + binaryRight + ')';
-    }
-    return binaryLeft+operator+binaryRight;
-}
-export {parseCode,getElementFromParseCode,makeTable};
-*/
-
-export {parseCode,makeTable};
+export {makeTable};
 
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import {makeTable} from '../src/js/code-analyzer';
 
 describe('regular function', () => {
-    it('is parsing an empty code correctly', () => {
+    it('just function', () => {
         assert.equal(
             JSON.stringify(makeTable('""','function foo(){}')),
             '"<tr><td>function foo(){</td></tr><tr><td>}</td></tr>"'
@@ -10,24 +10,249 @@ describe('regular function', () => {
     });
 });
 
-describe('regular function', () => {
-    it('is parsing an empty code correctly', () => {
+describe('function with condition if', () => {
+    it('just if', () => {
         assert.equal(
-            JSON.stringify(makeTable('""','function foo(){}')),
-            '"<tr><td>function foo(){</td></tr><tr><td>}</td></tr>"'
+            JSON.stringify(makeTable('x=1,y=2,z=3','function foo(x, y, z){\n' +
+                '    let a = x + 1;\n' +
+                '    let b = a + y;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    if (b < z) {\n' +
+                '        c = c + 5;\n' +
+                '        return x + y + z + c;\n' +
+                '}\n' +
+                '\n' +
+                '}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;if(x + 1 + y < z ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x + y + z + (0 + 5);</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with if', () => {
+    it('if return true', () => {
+        assert.equal(
+            JSON.stringify(makeTable('""','function foo(){\n' +
+                'let a =3;\n' +
+                'if(a>2){\n' +
+                'return true;\n' +
+                '}\n' +
+                '}')),
+            '"<tr><td>function foo(){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(3 > 2 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return true;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with condition if else if and else', () => {
+    it('if else if and else', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1,y=2,z=3','function foo(x, y, z){let a = x + 1;let b = a + y;let c = 0;if (b < z) {c = c + 5;return x + y + z + c;} else if (b < z * 2) {c = c + x + 5;return x + y + z + c;} else {c = c + z + 5;return x + y + z + c;}}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;if(x + 1 + y < z ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x + y + z + (0 + 5);</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;}else if(x + 1 + y < z * 2){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x + y + z + (0 + x + 5);</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}else{</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x + y + z + (0 + z + 5);</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with while', () => {
+    it('while return true', () => {
+        assert.equal(
+            JSON.stringify(makeTable('""','function foo(){\n' +
+                'let a =4;\n' +
+                'while(a>2){\n' +
+                'a--;\n' +
+                '}\n' +
+                'return true;\n' +
+                '}')),
+            '"<tr><td>function foo(){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;while(4 > 2){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return true;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with condition while green', () => {
+    it('while green', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1,y=2,z=3','function foo(x, y, z){\n' +
+                '    let a = x + 1;\n' +
+                '    let b = a + y;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    while (a < z) {\n' +
+                '        c = a + b;\n' +
+                '        z = c * 2;\n' +
+                '    }\n' +
+                '    \n' +
+                '    return z;\n' +
+                '}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;while(x + 1 < z){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;z=((x + 1) + (x + 1 + y)) * 2;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return z;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with condition while', () => {
+    it('while red', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1,y=2,z=1','function foo(x, y, z){\n' +
+                '    let a = x + 1;\n' +
+                '    let b = a + y;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    while (a < z) {\n' +
+                '        c = a + b;\n' +
+                '        z = c * 2;\n' +
+                '    }\n' +
+                '    \n' +
+                '    return z;\n' +
+                '}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;while(x + 1 < z){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;z=((x + 1) + (x + 1 + y)) * 2;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return z;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with condition while and  if', () => {
+    it('while red and  if green', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1,y=2,z=1','function foo(x, y, z){\n let a = x + 1;\n    let b = a + y;\nlet c = b +2;\n\nwhile (a < z) {\nif(c>3){\nc = a + b;\nz = c * 2;\n}\n\n}\n\nreturn z;\n}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;while(x + 1 < z){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if(x + 1 + y + 2 > 3 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;z=((x + 1) + (x + 1 + y)) * 2;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return z;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with condition if', () => {
+    it('if in if', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1,y=2,z=1','function foo(x, y, z){\nlet a = x + 1;\nlet b = a + y;\nlet c = b +2;\n\nif (a < z) {\nif(c>3){\nc = a + b;\nz = c * 2;\n}\n\n}\n\nreturn z;\n}')),
+            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;if(x + 1 < z ){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if(x + 1 + y + 2 > 3 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;z=((x + 1) + (x + 1 + y)) * 2;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return z;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with array', () => {
+    it('if in if', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x = [1,2,3]','function foo(x){\n' +
+                '    let a = x[0] + 1;\n' +
+                '    let b = x[2] + 3;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    if (b > a) {\n' +
+                '        c= x[1]\n' +
+                '    }\n' +
+                '    \n' +
+                '    return c;\n' +
+                '}\n')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x[2] + 3 > x[0] + 1 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c=x[1];</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return x[1];</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with array', () => {
+    it('array green', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x = [1,2,3]','function foo(x){\n' +
+                '    let a = x[0] + 1;\n' +
+                '    let b = x[2] + 3;\n' +
+                '    let c = 0;\n' +
+                '    \n' +
+                '    if (b > a) {\n' +
+                '        c= x[1]\n' +
+                '    }\n' +
+                '    \n' +
+                '    return c;\n' +
+                '}\n')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x[2] + 3 > x[0] + 1 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c=x[1];</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return x[1];</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with array', () => {
+    it('array red', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x = [1,2,3]','function foo(x){let a = x[2] + 1;let b = x[0] + 3;let c = 0;if (b > a) {c= x[1]}return c;}')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#FF0000><td>&nbsp;&nbsp;&nbsp;if(x[0] + 3 > x[2] + 1 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c=x[1];</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return x[1];</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with array boolean', () => {
+    it('boolean is green', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x = [true,false]','function foo(x){\n' +
+                '    let c =10;\n' +
+                '    if (x[0]) {\n' +
+                '        c= 100;\n' +
+                '    }\n' +
+                '    \n' +
+                '    return c;\n' +
+                '}')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x[0] ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c=100;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return 100;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with if', () => {
+    it('if variable deceleration', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=1','function foo(x){let c =10;if (x>0) {let a= 100;c = a}return c;}')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x > 0 ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c=100;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return 100;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with array and global var', () => {
+    it('array and golbal var', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x[1]','let a = 10;\nfunction foo(x){\n\nreturn a + x[0];\n}')),
+            '"<tr><td>function foo(x){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return (a) + x[0];</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('function with if an local variable', () => {
+    it('assignment variable in if ', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x=[3,5]','function foo(x){\n' +
+                'let a = x[1];\n' +
+                'let b = x[0];\n' +
+                'if(a>b){\n' +
+                'a=b;\n' +
+                '}\n' +
+                'return a;\n' +
+                '\n' +
+                '}\n')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x[1] > x[0] ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a=x[0];</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return x[0];</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('assignment of global variable', () =>     {
+    it('global variable', () => {
+        assert.equal(
+            JSON.stringify(makeTable('""','function foo(x){\n' +
+                'let a =4;\n' +
+                'while(a>2){\n' +
+                ' x = x + x + a +1\n' +
+                '}\n' +
+                'return x;\n' +
+                '}')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;while(4 > 2){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x=(x) + (x) + (4) + 1;</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>&nbsp;&nbsp;&nbsp;return x;</td></tr><tr><td>}</td></tr>"'
+        );
+    });
+});
+
+describe('check array', () => {
+    it('array with boolean and numbers', () => {
+        assert.equal(
+            JSON.stringify(makeTable('x = [true,2,4]','function foo(x){let a =4;if(x[0]){a = x[1] + a +x[2];return a;}}')),
+            '"<tr><td>function foo(x){</td></tr><tr bgcolor=#00FF00><td>&nbsp;&nbsp;&nbsp;if(x[0] ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x[1] + 4 + x[2];</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>}</td></tr>"'
         );
     });
 });
 
 
-describe('regular function', () => {
-    it('is parsing an empty code correctly', () => {
-        assert.equal(
-            JSON.stringify(makeTable('"x=1,y=2,z=3"','function foo(x, y, z){let a = x + 1;let b = a + y;let c = 0;if (b < z) {c = c + 5;return x + y + z + c;}}')),
-            '"<tr><td>function foo(x,y,z){</td></tr><tr bgcolor="red"><td>&nbsp;&nbsp;&nbsp;if(x + 1 + y < z ){</td></tr><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return x + y + z + (0 + 5);</td></tr><tr><td>&nbsp;&nbsp;&nbsp;}</td></tr><tr><td>}</td></tr>"'
-        );
-    });
-});
+
+
+
+
+
 
 
 
